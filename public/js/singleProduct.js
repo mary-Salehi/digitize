7c254fd8fa599ca60dd16c104d9877884cc081ca
maryamSalehi <maryam010salehi@gmail.com>
2024-12-02 11:12:27 +0330

    singlePtoduct file changed

--- public/js/singleProduct.js
+++ public/js/singleProduct.js
@@ -1,13 +1,18 @@
 'use strict'
+const tabsContainer = document.querySelector('.info__tab-container');
+const tabsContentContainer = document.querySelector('.info__contents');
+const tabsContent = document.querySelectorAll('.info__content');
+const tabsBtn = document.querySelectorAll('.info__tab');
 
-// import Swal from 'sweetalert2/dist/sweetalert2.js';
-// import 'sweetalert2/src/sweetalert2.scss';
 
-// Swal.fire("محصول یه سبد خرید اضافه ش");
-const addToCartBtn = document.querySelectorAll('.add-to-cart-btn');
-
-// addToCartBtn.forEach(function(btn){
-//     btn.addEventListener('click' , function(){
-//         Swal.fire("محصول یه سبد خرید اضافه ش");
-//     })
-// })
+tabsContainer.addEventListener('click' , function(e) {
+    const clicked = e.target;
+    console.log(clicked);
+    if (clicked.classList.contains('info__tab')) {
+        tabsBtn.forEach(tab => tab.classList.remove('text-slate-800' , 'bg-white', 'rounded-3xl', 'shadow-md'));
+        clicked.classList.add('text-slate-800' , 'bg-white', 'rounded-3xl', 'shadow-md');
+        console.log(tabsContentContainer);
+        tabsContent.forEach(content => content.classList.add('hidden'));
+        const targetContent = document.querySelector(`.info__content--${clicked.dataset.tab}`).classList.remove('hidden');
+    }
+})
